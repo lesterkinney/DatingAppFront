71f397b63df52038b4e2be13f3016d1996683451
Les Kinney <lester.kinney@gmail.com>
2021-03-04 17:29:55 -0700

    End of Section 17

--- src/app/_guards/prevent-unsaved-changes.guard.ts
+++ src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -8,7 +8,7 @@ import { MemberEditComponent } from '../members/member-edit/member-edit.componen
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
   canDeactivate(component: MemberEditComponent): boolean {
-    if(component.editForm.dirty) {
+    if(component.editForm?.dirty) {
       return confirm('You have unsaved changes, continue?');
     }
     return true;
